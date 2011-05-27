d5fe370abc8cfba437b9787402edfc0dc77120ae
Seth Webster <sethwebster@gmail.com>
2011-05-26 23:01:30 -0700

    Missed Notification class

---
+++ TweetHarbor/Models/Notification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TweetHarbor.Models
+{
+    public class Notification
+    {
+        public Application application { get; set; }
+        public Build build { get; set; }
+    }
+
+    public class Application
+    {
+        public string name { get; set; }
+    }
+
+    public class Build
+    {
+        public Commit commit { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Commit
+    {
+        public string id { get; set; }
+        public string message { get; set; }
+    }
+}
