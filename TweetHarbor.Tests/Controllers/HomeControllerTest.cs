9e48064913c6478319d0f57570b7cecc2bc4f6f9
Seth Webster <sethwebster@gmail.com>
2011-05-27 00:01:22 -0700

    Removing default tests

--- TweetHarbor.Tests/Controllers/HomeControllerTest.cs
+++ TweetHarbor.Tests/Controllers/HomeControllerTest.cs
@@ -12,30 +12,6 @@ namespace TweetHarbor.Tests.Controllers
     [TestClass]
     public class HomeControllerTest
     {
-        [TestMethod]
-        public void Index()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Index() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Welcome to ASP.NET MVC!", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
+       
     }
 }
