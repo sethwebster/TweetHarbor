52b08fc8769b5f9112b7b8559b06792e4df48021
sethwebster <sethwebster@gmail.com>
2011-05-31 23:51:28 -0700

    Cleaned up and refactored NotifyController - separated some methods & added some comments

--- TweetHarbor/Controllers/NotifyController.cs
+++ TweetHarbor/Controllers/NotifyController.cs
@@ -15,11 +15,11 @@ namespace TweetHarbor.Controllers
     {
         ITweetHarborDbContext database;
         ITweetHarborTwitterService twitter;
+
         public NotifyController(ITweetHarborDbContext database, ITweetHarborTwitterService twitter)
         {
             this.database = database;
             this.twitter = twitter;
-            
         }
 
         public ActionResult New()
@@ -30,62 +30,64 @@ namespace TweetHarbor.Controllers
         [HttpPost]
         public JsonResult New(string Id, string token, Notification notification)
         {
-            var u = database.Users
+            // Get the User based on supplied Id
+            // * Token must match *
+            var user = database.Users
                 .Include("Projects")
                 .Include("Projects.MessageRecipients")
                 .FirstOrDefault(usr => usr.TwitterUserName == Id && usr.UniqueId == token);
-            if (null != u)
+            // If Id or Token is invalid, user will not be found
+            // TODO: Allow users to reset the token
+            if (null != user)
             {
-                var project = u.Projects.FirstOrDefault(p => p.ProjectName == notification.application.name);
-                if (null == project)
-                {
-                    project = new Project() 
-                    {
-                        ProjectName = notification.application.name,
-                        FailureTemplate = "",
-                        SuccessTemplate = "",                       
-                        User = u
-                    };
-                    u.Projects.Add(project);
-                    //TODO: Add logging to prevent these errors from being swallowed
-                    database.SaveChanges();
-                }
+                // Locate or create our project
+                var project = CreateProjectIfNecessary(notification, user);
                 if (null != project)
                 {
-                    twitter.AuthenticateWith(u.OAuthToken, u.OAuthTokenSecret);
+                    // start our connection to twitter
+                    twitter.AuthenticateWith(user.OAuthToken, user.OAuthTokenSecret);
 
+                    // Format and send appropriate messages
                     if (notification.build.status == "succeeded")
                     {
-                        var strSuccessUpdate = string.IsNullOrEmpty(project.SuccessTemplate) ? Properties.Settings.Default.DefaultSuccessTemplate : project.SuccessTemplate;
-                        strSuccessUpdate = strSuccessUpdate.Replace("{application:name}", project.ProjectName)
-                            .Replace("{build:commit:message}", notification.build.commit.message)
-                            .Replace("{build:commit:id}", notification.build.commit.id);
+                        // Get the Success Template (or default)
+                        var strSuccessUpdate = string.IsNullOrEmpty(project.SuccessTemplate) ?
+                            Properties.Settings.Default.DefaultSuccessTemplate : project.SuccessTemplate;
+
+                        // Replace tokens
+                        strSuccessUpdate = DeTokenizeString(strSuccessUpdate, project, notification);
+
                         if (strSuccessUpdate.Length > 140)
                             strSuccessUpdate = strSuccessUpdate.Substring(0, 136) + "...";
 
-                        if (project.SendPrivateTweetOnSuccess && u.SendPrivateTweet)
+                        // ensure we're 'authorized' to send the tweet
+                        if (project.SendPrivateTweetOnSuccess && user.SendPrivateTweet)
                         {
-                           // TwitterDirectMessage dmRes = twitter.SendDirectMessage(u.TwitterUserName, strSuccessUpdate);
                             SendDirectMessages(project, strSuccessUpdate);
                         }
-                        if (project.SendPublicTweetOnSuccess && u.SendPublicTweet)
+                        if (project.SendPublicTweetOnSuccess && user.SendPublicTweet)
                         {
                             TwitterStatus pubRes = twitter.SendTweet(strSuccessUpdate);
                         }
                     }
                     else
                     {
-                        var strFailureUpdate = string.IsNullOrEmpty(project.FailureTemplate) ? Properties.Settings.Default.DefaultFailureTemplate : project.FailureTemplate;
-                        strFailureUpdate = strFailureUpdate.Replace("{application:name}", project.ProjectName).Replace("{build:commit:message}", notification.build.commit.message);
+                        // Get the Failure Template (or default)
+                        var strFailureUpdate = string.IsNullOrEmpty(project.FailureTemplate) ?
+                            Properties.Settings.Default.DefaultFailureTemplate : project.FailureTemplate;
+
+                        // Replace tokens & clean up 
+                        strFailureUpdate = DeTokenizeString(strFailureUpdate, project, notification);
+
                         if (strFailureUpdate.Length > 140)
                             strFailureUpdate = strFailureUpdate.Substring(0, 136) + "...";
 
-                        if (project.SendPrivateTweetOnFailure && u.SendPrivateTweet)
+                        // ensure we're 'authorized' to send the tweet
+                        if (project.SendPrivateTweetOnFailure && user.SendPrivateTweet)
                         {
-                          //  var dmRes = twitter.SendDirectMessage(u.TwitterUserName, strFailureUpdate);
                             SendDirectMessages(project, strFailureUpdate);
                         }
-                        if (project.SendPublicTweetOnFailure && u.SendPublicTweet)
+                        if (project.SendPublicTweetOnFailure && user.SendPublicTweet)
                         {
                             var pubRes = twitter.SendTweet(strFailureUpdate);
                         }
@@ -104,6 +106,36 @@ namespace TweetHarbor.Controllers
 
         }
 
+        // TODO: Create a richer tokenization approach (string replace isn't pretty, but worked)
+        [NonAction]
+        string DeTokenizeString(string input, Project project, Notification notification)
+        {
+            return input.Replace("{application:name}", project.ProjectName)
+                            .Replace("{build:commit:message}", notification.build.commit.message)
+                            .Replace("{build:commit:id}", notification.build.commit.id);
+        }
+
+        [NonAction]
+        private Project CreateProjectIfNecessary(Notification notification, Models.User user)
+        {
+            var project = user.Projects.FirstOrDefault(p => p.ProjectName == notification.application.name);
+            if (null == project)
+            {
+                project = new Project()
+                {
+                    ProjectName = notification.application.name,
+                    FailureTemplate = "",
+                    SuccessTemplate = "",
+                    User = user
+                };
+                user.Projects.Add(project);
+                //TODO: Add logging to prevent these errors from being swallowed
+                database.SaveChanges();
+            }
+            return project;
+        }
+
+        [NonAction]
         private void SendDirectMessages(Project project, string update)
         {
             foreach (var r in project.MessageRecipients)
@@ -111,10 +143,6 @@ namespace TweetHarbor.Controllers
                 twitter.SendDirectMessage(r.ScreenName, update);
             }
         }
-
-
-
-
 
         protected override void Dispose(bool disposing)
         {
