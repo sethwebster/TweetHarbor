91ed425c83fb12b081fa915b510b1c737423bf3c
Seth Webster <sethwebster@gmail.com>
2011-06-06 21:23:57 -0700

    Added some informative text

--- TweetHarbor/Models/JsonResultModel.cs
+++ TweetHarbor/Models/JsonResultModel.cs
@@ -7,7 +7,17 @@ namespace TweetHarbor.Models
 {
     public class JsonResultModel
     {
+        /// <summary>
+        /// A boolean value indicating the operation was a success 
+        /// </summary>
         public bool Success { get; set; }
+        /// <summary>
+        /// The diagnostic error message
+        /// </summary>
         public string Error { get; set; }
+        /// <summary>
+        /// The status message
+        /// </summary>
+        public string Message { get; set; }
     }
 }
