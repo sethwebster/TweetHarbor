395bc44a0b1e36067cc23b1b93a8a4837fdd306f
sethwebster <sethwebster@gmail.com>
2011-11-12 12:58:17 -0700

    --amend

---
+++ TweetHarbor/OAuth/AppHarborClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TweetHarbor.OAuth
+{
+    public class AppHarborClient
+    {
+        string clientId = string.Empty;
+        public AppHarborClient(string clientId)
+        {
+            this.clientId = clientId;
+        }
+        public Uri GetAuthorizationUrl()
+        {
+            return new Uri(string.Format("https://appharbor.com/user/authorizations/new?client_id={0}", clientId));
+        }
+
+        public RedirectResult RedirectToAuthorizationResult()
+        {
+            return new RedirectResult(GetAuthorizationUrl().ToString());
+        }
+
+
+    }
+}
