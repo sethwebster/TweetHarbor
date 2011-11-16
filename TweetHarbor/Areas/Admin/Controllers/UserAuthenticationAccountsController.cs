82a31db872f89eb79e8c459a7ca5bf8355dde41a
sethwebster <sethwebster@gmail.com>
2011-11-16 13:42:04 -0700

    Added some user management capabilities

---
+++ TweetHarbor/Areas/Admin/Controllers/UserAuthenticationAccountsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TweetHarbor.Data;
+
+namespace TweetHarbor.Areas.Admin.Controllers
+{
+    public class UserAuthenticationAccountsController : Controller
+    {
+        ITweetHarborDbContext database;
+
+        public UserAuthenticationAccountsController(ITweetHarborDbContext database)
+        {
+            this.database = database;
+        }
+
+        public ActionResult Index()
+        {
+            return View(database.UserAuthenticationAccounts.OrderBy(a => a.UserName));
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            database.UserAuthenticationAccounts.Remove(
+                database.UserAuthenticationAccounts.FirstOrDefault(u => u.UserAuthenticationAccountId == Id)
+                );
+            database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
